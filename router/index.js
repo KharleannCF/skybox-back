3d5a57be5c54df44bea97998a08cb9f5d35d09d9
kharleanncf <kharleann@gmail.com>
2021-06-17 18:48:12 -0400

    idk;

--- router/index.js
+++ router/index.js
@@ -2,14 +2,19 @@ const router = require("express").Router();
 const sceneController = require("../controllers/scenes");
 
 router.get("/scenes", async (req, res) => {
-	const scenes = await sceneController.getScenes();
-	res.status(200).json({ ok: true, scenes });
+  const scenes = await sceneController.getScenes();
+  res.status(200).json({ ok: true, scenes });
 });
 
 router.get("/scenes/getImages/:name", async (req, res) => {
-	console.log("asdasdasdasdasd")
-	const scenes = await sceneController.getImages(req.params.name);
-	res.status(200).json({ ok: true, scenes });
+  console.log("asdasdasdasdasd");
+  const scenes = await sceneController.getImages(req.params.name);
+  res.status(200).json({ ok: true, scenes });
+});
+
+router.get("/test2", async (req, res) => {
+  console.log("asdasdasdasdasd");
+  res.status(200).json({ ok: true });
 });
 
 module.exports = router;
